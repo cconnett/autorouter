665b54d172d13ae98f474856ef181673f77b6e9b
Chris Connett <chris@connett.net>
2019-11-10 20:20:43 -0500

    Hacky version that does SOP.

--- autorouter.py
+++ autorouter.py
@@ -1,8 +1,9 @@
 # python3
 """Route maker and optimizer for Metroid/Zelda/Castlevania style games."""
 
+import sys
+
 from absl import app
-# import astar
 import elkai
 import networkx
 import numpy as np
@@ -28,6 +29,19 @@ def LoadMap():
   return themap
 
 
+def Validate(items, where, locations):
+  unplaced_items = items - set(where.keys())
+  if unplaced_items:
+    raise ValueError(
+        f'The following {len(unplaced_items)} items have no location:\n' +
+        '\n'.join(f'  {it}' for it in sorted(unplaced_items)))
+  unknown_locations = set(where.values()) - locations
+  if unknown_locations:
+    raise ValueError("The following locations aren't on the map:\n" + '\n'.join(
+        f'  {l}' for l in sorted(unknown_locations)))
+  assert not locations & where.keys(), locations & where.keys()
+
+
 def main(unused_argv):
   dependencies = networkx.drawing.nx_pydot.read_dot(
       'data/AgentA/dependencies.dot')
@@ -36,57 +50,78 @@ def main(unused_argv):
 
   items = set(dependencies.nodes) | set(where.keys())
   locations = set(themap.nodes)
+  blocking_locations = set(dependencies.nodes) & locations
   items -= locations
 
-  unplaced_items = items - set(where.keys())
-  if unplaced_items:
-    raise ValueError(
-        f'The following {len(unplaced_items)} items have no location:\n' +
-        '\n'.join(f'  {it}' for it in sorted(unplaced_items)))
-  unknown_locations = set(where.values()) - locations
-  if unknown_locations:
-    raise ValueError("The following locations aren't on the map:\n" +
-                     '\n'.join(f'  {l}' for l in sorted(unknown_locations)))
+  Validate(items, where, locations)
 
-  items = list(items)
+  items = sorted(items)
   start = items.index('start')
   end = items.index('end')
   items[0], items[start] = items[start], items[0]
   items[-1], items[end] = items[end], items[-1]
 
   apsp = dict(
-      networkx.algorithms.shortest_paths.weighted
-      .all_pairs_bellman_ford_path_length(
+      networkx.algorithms.shortest_paths.weighted.
+      all_pairs_bellman_ford_path_length(
           themap, weight=lambda _1, _2, attrs: int(attrs.get('weight', 1))))
-  item_precedence = dict(networkx.all_pairs_shortest_path_length(dependencies))
+  apsp_path = dict(
+      networkx.algorithms.shortest_paths.weighted.all_pairs_bellman_ford_path(
+          themap, weight=lambda _1, _2, attrs: int(attrs.get('weight', 1))))
   costs = np.zeros((len(items), len(items)), int)
   for i in range(len(items)):
     for j in range(len(items)):
       if i == j:
-        costs[i, j] = INFINITY
-        continue
+        costs[i, j] = 0
+      elif items[i] == 'end' or items[j] == 'start':
+        costs[i, j] = -1
+      else:
+        costs[i, j] = apsp[where[items[i]]][where[items[j]]]
+  del i, j
+  for item_i in items:
+    blockable_locations_start_to_i = (
+        set(apsp_path[where['start']][where[item_i]]) & blocking_locations)
+    for block in blockable_locations_start_to_i:
+      themap2 = themap.copy()
+      themap2.remove_node(block)
       try:
-        cost = apsp[where[items[i]]][where[items[j]]]
-        costs[i, j] = (cost if cost > 0 else INFINITY)
-        if items[i] == 'end':
-          costs[i, j] = INFINITY
-        if items[i] == 'start':
-          continue
-        if (items[i] in item_precedence and
-            items[j] not in item_precedence[items[i]]):
-          costs[i, j] = INFINITY
-      except:
-        print(items[i], items[j])
-        print(where[items[i]], where[items[j]])
-        import pdb
-        pdb.post_mortem()
+        # Check if a path still exists.
+        if where[item_i] == block:
+          raise networkx.NetworkXNoPath
+        networkx.algorithms.shortest_paths.generic.shortest_path(
+            themap2, where['start'], where[item_i])
+      except networkx.NetworkXNoPath:
+        for requirement in dependencies[block]:
+          # print(f'start->{item_i} needs {requirement} because it blocks {block}')
+          dependencies.add_edge(item_i, requirement)
 
-  tour = elkai.solve_int_matrix(costs)
-  for index, prev in zip(tour[1:], tour):
-    print(
+  item_precedence = dict(networkx.all_pairs_shortest_path_length(dependencies))
+  for item_i in item_precedence:
+    if item_i not in items:
+      continue
+    for item_j in item_precedence[item_i]:
+      # print(f'Cannot go from {item_i} to {item_j}.')
+      costs[items.index(item_i), items.index(item_j)] = -1
+
+  if len(sys.argv) == 1:
+    print('NAME: AgentA')
+    print('TYPE: SOP')
+    print('EDGE_WEIGHT_TYPE: EXPLICIT')
+    print('EDGE_WEIGHT_FORMAT: FULL_MATRIX')
+    print(f'DIMENSION: {len(items)}')
+    print('EDGE_WEIGHT_SECTION')
+    print(len(items))
+    for i in range(len(items)):
+      for j in range(len(items)):
+        print(f'{costs[i,j]:<4d} ', end='')
+      print()
+  else:
+    tourdata = open(sys.argv[1]).readlines()
+    tour = [int(node) - 1 for node in tourdata[6:6 + len(items)]]
+    for index, prev in zip(tour[1:], tour):
+      print(
         f'Go {costs[prev, index]} steps to {where[items[index]]} and get {items[index]}.'
-    )
-
+      )
 
 if __name__ == '__main__':
   app.run(main)
